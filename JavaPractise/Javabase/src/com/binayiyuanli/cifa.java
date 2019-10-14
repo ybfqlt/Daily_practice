1fe28522c687a3b695c7625100032865345e315b
ltt <823030735@qq.com>
2019-10-14 21:11:28 +0800

    词法分析器

--- JavaPractise/Javabase/src/com/binayiyuanli/cifa.java
+++ JavaPractise/Javabase/src/com/binayiyuanli/cifa.java
@@ -2,6 +2,7 @@ package com.binayiyuanli;
 
 
 import java.io.*;
+import java.util.Scanner;
 
 /**
  * @Classname cifa
@@ -10,8 +11,12 @@ import java.io.*;
  * @Created by xns
  */
 public class cifa {
-    private String keyWord[] = {"auto","break","case","char","const","continue","default","do","double","else","enum","float","for","goto","if","int","long","register","return","short","signed","sizeof","static","struct", "switch","typedef","union","unsigned","void","volatile","while"};;
+    private String keyWord[] = {"auto","break","case","char","const","continue","default",
+            "do","double","else","enum","float","for","goto","if","int","long","register","return","short",
+            "signed","sizeof","static","struct", "switch","typedef","union","unsigned","void","volatile","while"};;
     private char ch;
+
+    private String format[]={"％d","％o","％x","％u","％c","％s","％f","％e","％g"};
 
     /**
      * 判断是否是关键字
@@ -57,6 +62,30 @@ public class cifa {
         }
     }
 
+    void filter(char[] str) {
+        String buf = "";
+        int count = 0;
+        for (int i = 0; i < str.length; i++) {
+            if (str[i] == '/' && str[i + 1] == '/') {
+                while (str[i] != '\n') {
+                    i++;
+                }
+            }
+            if (str[i] == '/' && str[i + 1] == '*') {
+                i += 2;
+                while (str[i] != '*' || str[i + 1] != '/') {
+                    i++;
+                    if (i == str.length) {
+                        System.out.println("注释出错，不能正确分析!");
+                        System.exit(0);
+                    }
+                }
+                i += 2;
+            }
+            buf = buf + str[i];
+        }
+    }
+
     /**
      * 词法分析
      * @param str
@@ -77,11 +106,11 @@ public class cifa {
                 i--;
                 if(isKey(arr)){
                     //关键字
-                    System.out.println(arr+"\t4"+"\t关键字");
+                    System.out.println("<"+arr+" ,"+"  关键字>");
                 }
                 else{
                     //标识符
-                    System.out.println(arr+"\t4"+"\t标识符");
+                    System.out.println("<"+arr+" ,"+"  标识符>");
                 }
             }
             else if(isDigit(ch)||(ch == '.'))
@@ -95,93 +124,129 @@ public class cifa {
                     ch = str[++i];
                 }
                 //属于无符号常数
-                System.out.println(arr+"\t5"+"\t常数");
+                System.out.println("<"+arr+" ,"+"  常数>");
             }
             else {
                 switch(ch){
                         /**
                         * 运算符
                         */
-                        case '+':System.out.println(ch+"\t2"+"\t运算符");break;
-                        case '-':System.out.println(ch+"\t2"+"\t运算符");break;
-                        case '*':System.out.println(ch+"\t2"+"\t运算符");break;
-                        case '/':System.out.println(ch+"\t2"+"\t运算符");break;
+                        case '%':
+                            String temp1="";
+                            int t=i;
+                            char c = str[t++];
+                            while(c!=')'){
+                                temp1+=c;
+                                c = str[t++];
+                            }
+                            if(temp1.contains("\"")){
+                                String temp2 = ""+ch+str[++i];
+                                System.out.println("<"+temp2+" ,"+" 格式表明符>");
+                            }
+                            else{
+                                System.out.println("<"+ch+" ,"+"  运算符>");
+                            }
+                            break;
+                        case '+':
+                        case '-':
+                        case '*':
+                        case '/':
+                            String temp;
+                            temp=""+ch;
+                            ch=str[++i];
+                            if(ch == '=') {
+                                temp=temp+ch;
+                            } else {
+                                ch = str[--i];
+                                System.out.println("<"+ch+" ,"+"  运算符>");
+                            }
+                            System.out.println("<"+temp+" ,"+"  运算符>");break;
                         /**
                         * 分界符
                         */
-                        case '#':System.out.println(ch+"\t3"+"\t界符");break;
-                        case '(':System.out.println(ch+"\t3"+"\t界符");break;
-                        case ')':System.out.println(ch+"\t3"+"\t界符");break;
-                        case '[':System.out.println(ch+"\t3"+"\t界符");break;
-                        case ']':System.out.println(ch+"\t3"+"\t界符");break;
-                        case ';':System.out.println(ch+"\t3"+"\t界符");break;
-                        case '{':System.out.println(ch+"\t3"+"\t界符");break;
-                        case '}':System.out.println(ch+"\t3"+"\t界符");break;
+                        case ',':
+                        case '"':
+                        case '#':
+                        case '(':
+                        case ')':
+                        case '[':
+                        case ']':
+                        case ';':
+                        case '{':
+                        case '}':System.out.println("<"+ch+" ,"+"  界符>");break;
                         /**
                         * 运算符
                         */
                         case '=':{
                             ch = str[++i];
                             if(ch == '=') {
-                                System.out.println("=="+"\t2"+"\t运算符");
+                                System.out.println("<=="+" ,"+"  运算符>");
                             } else {
-                                System.out.println("="+"\t2"+"\t运算符");
+                                System.out.println("<"+"="+" ,"+"  运算符>");
                                 i--;
                             }
                         }break;
                         case ':':{
                             ch = str[++i];
                             if(ch == '=') {
-                                System.out.println("="+"\t2"+"\t运算符");
+                                System.out.println("<"+":="+" ,"+"  运算符>");
                             } else {
-                                System.out.println("(:"+"\t2"+"\t运算符");
+                                System.out.println("<"+":"+" ,"+"  运算符>");
                                 i--;
                             }
                         }break;
                         case '>':{
                             ch = str[++i];
                             if(ch == '=') {
-                                System.out.println("("+ch+","+"\t运算符");
+                                System.out.println("<"+">="+" ,"+"  运算符>");
                             } else {
-                                System.out.println("("+ch+","+"\t运算符");
+                                System.out.println("<"+">"+" ,"+"  运算符>");
                                 i--;
                             }
                         }break;
                         case '<':{
                             ch = str[++i];
                             if(ch == '=') {
-                                System.out.println("("+ch+","+"\t运算符)");
+                                System.out.println("<"+"<="+" ,"+"  运算符>");
                             } else {
-                                System.out.println("("+ch+","+"\t运算符)");
+                                System.out.println("<"+"<"+" ,"+"  运算符>");
                                 i--;
                             }
                         }break;
                         /**
                         * 无识别
                         */
-                        default: System.out.println("("+ch+","+"\t无识别符");
+                        default: System.out.println("<"+ch+" ,"+"  无识别符>");
                     }
             }
         }
     }
     public static void main(String[] args) throws Exception {
+        Scanner in = new Scanner(System.in);
+        System.out.println("请输入文件名称:");
+        String strFile = in.next();
         /**
          * 定义一个file对象，用来初始化FileReader
          */
-        File file = new File("/home/ltt/IdeaProjects/JavaPractise/Javabase/src/com/binayiyuanli/s.txt");
-        /**
-         * 定义一个fileReader对象，用来初始化BufferedReader
-         */
-        FileReader reader = new FileReader(file);
-        int length = (int) file.length();
-        /**
-         * 这里定义字符数组的时候需要多定义一个,因为词法分析器会遇到超前读取一个字符的时候，如果是最后一个
-         * 字符被读取，如果在读取下一个字符就会出现越界的异常
-         */
-        char buf[] = new char[length+1];
-        reader.read(buf);
-        reader.close();
-        new cifa().analyze(buf);
+        try {
+            File file = new File(strFile);
+            /**
+             * 定义一个fileReader对象，用来初始化BufferedReader
+             */
+            FileReader reader = new FileReader(file);
+            int length = (int) file.length();
+            /**
+             * 这里定义字符数组的时候需要多定义一个,因为词法分析器会遇到超前读取一个字符的时候，如果是最后一个
+             * 字符被读取，如果在读取下一个字符就会出现越界的异常
+             */
+            char buf[] = new char[length+1];
+            reader.read(buf);
+            reader.close();
+            new cifa().analyze(buf);
+        }catch (FileNotFoundException e){
+            System.out.println("没有那个文件或目录!");
+        }finally {
 
+        }
     }
 }
