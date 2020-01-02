1e2117232e1ba7f90887d391a404a3d5b2ac7942
ltt <823030735@qq.com>
2020-01-02 21:35:01 +0800

    Js练习

---
+++ Java_practise_test/src/com/xns/little/test1.java
@@ -0,0 +1,17 @@
+package com.xns.little;
+
+/**
+ * @Classname test1
+ * @Description TODO
+ * @Date 19-12-26 下午11:02
+ * @Created by xns
+ */
+public class test1 {
+    private float f = 1.0f;
+    int m = 12;
+    static int n = 1;
+
+    public static void main(String[] args) {
+        test1 t = new test1();
+    }
+}
