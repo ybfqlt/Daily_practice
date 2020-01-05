b6cbe2632dec01b51349782fdf5bba89001cf60c
ltt <823030735@qq.com>
2020-01-05 22:33:36 +0800

    JavaEE考试练习

---
+++ JavaPractise/javaEE_demo1/src/main/java/com/test/A.java
@@ -0,0 +1,12 @@
+package com.test;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+/**
+ * @Author: xns
+ * @Date: 20-1-5 下午9:54
+ */
+public class A extends HttpServlet {
+
+}
