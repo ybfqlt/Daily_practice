676c253c0429c031bd9b2365a11e60a708e64140
ltt <823030735@qq.com>
2020-01-16 16:36:31 +0800

    服务发现

--- example_spring-cloud/servicecloud-provider-dept-8001/src/main/java/com/xns/springcloud/DeptProvider8001_App.java
+++ example_spring-cloud/servicecloud-provider-dept-8001/src/main/java/com/xns/springcloud/DeptProvider8001_App.java
@@ -4,6 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ImportResource;
 
@@ -13,6 +14,7 @@ import org.springframework.context.annotation.ImportResource;
  */
 @SpringBootApplication
 @EnableEurekaClient //本服务启动会自动注册进eureka服务中
+@EnableDiscoveryClient   //服务发现
 @MapperScan("com.xns.springcloud.dao")
 public class DeptProvider8001_App {
     public static void main(String[] args) {
--- example_spring-cloud/servicecloud-provider-dept-8001/src/main/java/com/xns/springcloud/controller/DeptController.java
+++ example_spring-cloud/servicecloud-provider-dept-8001/src/main/java/com/xns/springcloud/controller/DeptController.java
@@ -3,6 +3,8 @@ package com.xns.springcloud.controller;
 import com.xns.springcloud.entities.Dept;
 import com.xns.springcloud.service.DeptService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,6 +19,9 @@ public class DeptController {
     @Autowired
     DeptService deptService;
 
+    @Autowired
+    private DiscoveryClient client;
+
     @RequestMapping(value = "/dept/add",method = RequestMethod.POST)
     public boolean add(@RequestBody Dept dept){
         return deptService.add(dept);
@@ -30,5 +35,17 @@ public class DeptController {
     @RequestMapping(value = "/dept/list",method = RequestMethod.GET)
     public List<Dept> list(){
         return deptService.list();
+    }
+
+    @RequestMapping(value = "/dept/discovery",method = RequestMethod.GET)
+    public Object discovery(){
+        List<String> list = client.getServices();
+        System.out.println("************"+list);
+
+        List<ServiceInstance> serviceInstances = client.getInstances("SERVICECLOUD-DEPT");
+        for(ServiceInstance serviceInstance:serviceInstances){
+            System.out.println(serviceInstance.getServiceId()+"\t"+serviceInstance.getHost()+"\t"+serviceInstance.getPort()+"\t"+serviceInstance.getUri());
+        }
+        return this.client;
     }
 }
