855b507715440bd4889fa4271149d0f022261510
ltt <823030735@qq.com>
2020-01-19 18:28:28 +0800

    服务监控

---
+++ example_spring-cloud/servicecloud-consumer-hystrix-dashboard/src/main/java/com/xns/springcloud/DeptConsumer_DashBoard_App.java
@@ -0,0 +1,17 @@
+package com.xns.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * @Author: xns
+ * @Date: 20-1-18 下午8:39
+ */
+@SpringBootApplication
+@EnableHystrixDashboard
+public class DeptConsumer_DashBoard_App {
+    public static void main(String[] args) {
+        SpringApplication.run(DeptConsumer_DashBoard_App.class,args);
+    }
+}
