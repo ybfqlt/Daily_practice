36e0e66705cec114c2851fbf10bf5dc23c18bcba
ltt <823030735@qq.com>
2020-01-21 15:56:53 +0800

    config配置之客户端

---
+++ example_spring-cloud/servicecloud-config-client-3355/src/main/java/com/xns/springcloud/ConfigClient_3355_StartSpringCloudApp.java
@@ -0,0 +1,15 @@
+package com.xns.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: xns
+ * @Date: 20-1-21 下午3:26
+ */
+@SpringBootApplication
+public class ConfigClient_3355_StartSpringCloudApp {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClient_3355_StartSpringCloudApp.class,args);
+    }
+}
